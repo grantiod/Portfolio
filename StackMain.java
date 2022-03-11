000b516c03dee1ed89b6867aa4e2fbc31a1fbf2d
grantiod <giodice1212@gmail.com>
2022-03-11 21:42:45 +0100

    Added StackMain

---
+++ StackMain.java
@@ -0,0 +1,12 @@
+public class StackMain {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+
+        s.print();
+    }
+}
