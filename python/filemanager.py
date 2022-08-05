0d294377be656f5e3af730104fb4ec73dd0aec66
grantiod <giodice1212@gmail.com>
2022-08-05 17:38:24 -0400

    added calculator to desktop helper

--- python/filemanager.py
+++ python/filemanager.py
@@ -1,4 +1,4 @@
-from tkinter import END, Entry, Label, Tk, Button, mainloop, W, E, Text
+from tkinter import END, Entry, Label, Tk, Button, Toplevel, mainloop, W, E, Text
 import os
 
 root = Tk()
@@ -111,12 +111,115 @@ def change_dir():
     except (Exception):
         return
 
-# middle of top of screen
-os.chdir('C:/Users/giodi/OneDrive/Desktop/')
-current_directory_lbl = Label(root, text='Current Directory:', font='bold')
-current_directory_lbl.grid(row=0, column=0, columnspan=2, pady=10, sticky=W+E)
-current_directory = Label(root, text=os.getcwd())
-current_directory.grid(row=1, column=0, columnspan=2, pady=10)
+def calculator_win():
+    calc_win = Toplevel()
+    e = Entry(calc_win, width=35, borderwidth=5)
+    e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+    def button_click(number):
+        # e.delete(0, END)
+        current = e.get()
+        e.delete(0, END)
+        e.insert(0, str(current) + str(number))
+
+    def button_clearAll():
+        e.delete(0, END)
+
+    def button_addVals():
+        first_number = e.get()
+        global f_num
+        global math
+        math = "addition"
+        f_num = int(first_number)
+        e.delete(0, END)
+
+    def button_subVals():
+        first_number = e.get()
+        global f_num
+        global math
+        math = "subtraction"
+        f_num = int(first_number)
+        e.delete(0, END)
+
+    def button_mulVals():
+        first_number = e.get()
+        global f_num
+        global math
+        math = "multiplication"
+        f_num = int(first_number)
+        e.delete(0, END)
+
+    def button_divVals():
+        first_number = e.get()
+        global f_num
+        global math
+        math = "division"
+        f_num = int(first_number)
+        e.delete(0, END)
+
+    def button_equals():
+        if math == "addition":
+            second_number = e.get()
+            e.delete(0, END)
+            e.insert(0, f_num + int(second_number))
+        elif math == "subtraction":
+            second_number = e.get()
+            e.delete(0, END)
+            e.insert(0, f_num - int(second_number))
+        elif math == "multiplication":
+            second_number = e.get()
+            e.delete(0, END)
+            e.insert(0, f_num * int(second_number))
+        elif math == "division":
+            second_number = e.get()
+            e.delete(0, END)
+            e.insert(0, f_num / int(second_number))
+
+    # define buttons
+    if True:
+        button_1 = Button(calc_win, text="1", padx=40, pady=20, command=lambda: button_click(1))
+        button_2 = Button(calc_win, text="2", padx=40, pady=20, command=lambda: button_click(2))
+        button_3 = Button(calc_win, text="3", padx=40, pady=20, command=lambda: button_click(3))
+        button_4 = Button(calc_win, text="4", padx=40, pady=20, command=lambda: button_click(4))
+        button_5 = Button(calc_win, text="5", padx=40, pady=20, command=lambda: button_click(5))
+        button_6 = Button(calc_win, text="6", padx=40, pady=20, command=lambda: button_click(6))
+        button_7 = Button(calc_win, text="7", padx=40, pady=20, command=lambda: button_click(7))
+        button_8 = Button(calc_win, text="8", padx=40, pady=20, command=lambda: button_click(8))
+        button_9 = Button(calc_win, text="9", padx=40, pady=20, command=lambda: button_click(9))
+        button_0 = Button(calc_win, text="0", padx=40, pady=20, command=lambda: button_click(0))
+        button_add = Button(calc_win, text="+", padx=39, pady=20, command=button_addVals)
+        button_subtract = Button(calc_win, text="-", padx=41, pady=20, command=button_subVals)
+        button_multiply = Button(calc_win, text="x", padx=40, pady=20, command=button_mulVals)
+        button_divide = Button(calc_win, text="/", padx=41, pady=20, command=button_divVals)
+        button_equal = Button(calc_win, text="=", padx=90, pady=20, command=button_equals)
+        button_clear = Button(calc_win, text="Clear", padx=79, pady=20, command=button_clearAll)
+
+    # put buttons on screen
+    if True:
+        button_1.grid(row=3, column=0)
+        button_2.grid(row=3, column=1)
+        button_3.grid(row=3, column=2)
+        button_4.grid(row=2, column=0)
+        button_5.grid(row=2, column=1)
+        button_6.grid(row=2, column=2)
+        button_7.grid(row=1, column=0)
+        button_8.grid(row=1, column=1)
+        button_9.grid(row=1, column=2)
+        button_0.grid(row=4, column=0)
+        button_clear.grid(row=4, column=1, columnspan=2)
+        button_add.grid(row=5, column=0)
+        button_subtract.grid(row=6, column=0)
+        button_multiply.grid(row=6, column=1)
+        button_divide.grid(row=6, column=2)
+        button_equal.grid(row=5, column=1, columnspan=2)
+
+# current directory
+if True:
+    os.chdir('C:/Users/giodi/OneDrive/Desktop/')
+    current_directory_lbl = Label(root, text='Current Directory:', font='bold')
+    current_directory_lbl.grid(row=0, column=0, columnspan=2, pady=10, sticky=W+E)
+    current_directory = Label(root, text=os.getcwd())
+    current_directory.grid(row=1, column=0, columnspan=2, pady=10)
 
 # display files
 files_and_folders = os.listdir( os.getcwd() )
@@ -128,16 +231,23 @@ for x in files_and_folders:
         files_list.append(x)
     else:
         folders_list.append(x)
-list_files_lbl = Label(root, text='Files:')
-list_files_lbl.grid(row=3, column=0, pady=10, sticky=W+E)
-files_text_box = Text(root, width=50)
-files_text_box.insert(END, files_list)
-files_text_box.grid(row=4, column=0, pady=10, sticky=W+E)
-list_folders_lbl = Label(root, text='Folders:')
-list_folders_lbl.grid(row=3, column=1, pady=10)
-folders_text_box = Text(root, width=50)
-folders_text_box.insert(END, folders_list)
-folders_text_box.grid(row=4, column=1, pady=10, sticky=W+E)
+
+# files and folders
+if True:
+    list_files_lbl = Label(root, text='Files:')
+    list_files_lbl.grid(row=3, column=0, pady=10, sticky=W+E)
+    files_text_box = Text(root, width=50)
+    files_text_box.insert(END, files_list)
+    files_text_box.grid(row=4, column=0, pady=10, sticky=W+E)
+    list_folders_lbl = Label(root, text='Folders:')
+    list_folders_lbl.grid(row=3, column=1, pady=10)
+    folders_text_box = Text(root, width=50)
+    folders_text_box.insert(END, folders_list)
+    folders_text_box.grid(row=4, column=1, pady=10, sticky=W+E)
+
+# calculator pop-up page
+calculator_btn = Button(root, text='Calculator', command=calculator_win)
+calculator_btn.grid(row=0, column=2)
 
 # change directory
 if True:
--- python/weather-app.py
+++ python/weather-app.py
@@ -2,12 +2,16 @@ from tkinter import Entry, Button, Label, Tk, W, E, N, S
 import requests
 import json
 
+WEATHER_API_KEY = "5711fdcec2171844b445fb2bc3a15f7c"
+
 root = Tk()
 root.title("Weather App")
-root.geometry("600x100")
+root.geometry("600x200")
 
-# URL:
+# URL's:
 # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=21204&distance=5&API_KEY=F13BA867-EB7C-4EFC-B552-277B9AC861CC
+# https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+# https://api.openweathermap.org/data/2.5/weather?lat=39&lon=76&appid=5711fdcec2171844b445fb2bc3a15f7c
 
 # create zipLookup function
 def zipLookup():
@@ -15,35 +19,61 @@ def zipLookup():
     # zip_lbl.grid(row=1, column=0, columnspan=2)
 
     try:
-        # returns json file
-        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zip.get() + "&distance=5&API_KEY=F13BA867-EB7C-4EFC-B552-277B9AC861CC")
-        api = json.loads(api_request.content)
-        city = api[0]['ReportingArea']
-        quality = api[0]['AQI']
-        category = api[0]['Category']['Name']
+        # air quality api request
+        air_api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zip_entry.get() + "&distance=5&API_KEY=F13BA867-EB7C-4EFC-B552-277B9AC861CC")
+        print('air api requested')
+        air_api = json.loads(air_api_request.content)
+        print('air api json received')
+        city = air_api[0]['ReportingArea']
+        air_quality = air_api[0]['AQI']
+        air_category = air_api[0]['Category']['Name']
+        lat = air_api[0]['Latitude']
+        print(lat)
+        lon = air_api[0]['Longitude']
+        print(lon)
 
-        if category == "Good":
+        # weather api request
+        weather_api_request = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + WEATHER_API_KEY)
+        weather_api = json.loads(weather_api_request.content)
+        weather = weather_api['weather'][0]['main']
+        print('weather obtained')
+        temperature = weather_api['main']['temp']
+        print('temp obtained')
+        wind_speed = weather_api['wind']['speed']
+        print('wind speed obtained')
+        humidity = weather_api['main']['humidity']
+        print('humidity obtained')
+
+        if air_category == "Good":
             weather_color = "#0C0"
-        elif category == "Moderate":
+        elif air_category == "Moderate":
             weather_color = "#FFFF00"
-        elif category == "Unhealthy for Sensitive Groups":
+        elif air_category == "Unhealthy for Sensitive Groups":
             weather_color = "#ff9900"
-        elif category == "Unhealthy":
+        elif air_category == "Unhealthy":
             weather_color = "#FF0000"
-        elif category == "Very Unhealthy":
+        elif air_category == "Very Unhealthy":
             weather_color = "#99066"
-        elif category == "Hazardous":
+        elif air_category == "Hazardous":
             weather_color = "#660000"
 
         root.configure(background=weather_color)
-        lbl = Label(root, text=city + " Air Quality " + str(quality) + " " + category, font=("Helvetica", 20), background=weather_color)
-        lbl.grid(row=1, column=0, sticky=W+E)
+        air_lbl = Label(root, text=city + " Air Quality " + str(air_quality) + " " + air_category, font=("Helvetica", 20), background=weather_color)
+        air_lbl.grid(row=1, column=0, sticky=W+E)
+        weather_lbl = Label(root, text=weather, font=("Helvetica", 20), background=weather_color)
+        weather_lbl.grid(row=2, column=0, sticky=W+E)
+        temperature_lbl = Label(root, text=str(temperature), font=("Helvetica", 20), background=weather_color)
+        temperature_lbl.grid(row=3, column=0, sticky=W+E)
+        wind_speed_lbl = Label(root, text='Wind Speed' + str(wind_speed), font=("Helvetica", 20), background=weather_color)
+        wind_speed_lbl.grid(row=4, column=0, sticky=W+E)
+        humidity_lbl = Label(root, text='Humidity' + str(humidity), font=("Helvetica", 20), background=weather_color)
+        humidity_lbl.grid(row=5, column=0, sticky=W+E)
+
     except Exception as e:
-        api = "Error..."
+        print("Error...")
 
-zip = Entry(root)
-zip.grid(row=0, column=0, sticky=W+E+N+S, padx=5)
-
+zip_entry = Entry(root)
+zip_entry.grid(row=0, column=0, sticky=W+E+N+S, padx=5)
 zip_button = Button(root, text="Lookup Zipcode", command=zipLookup)
 zip_button.grid(row=0, column=1, sticky=W+E+N+S, padx=5)
 
